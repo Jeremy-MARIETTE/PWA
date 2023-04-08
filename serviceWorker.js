72b643c5611e4cc1a3808082c72c97298018f3a9
Kevin Chan <104557288+KevinChanGH@users.noreply.github.com>
2023-04-08 14:36:03 +0200

    ios support

---
+++ serviceWorker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event=>{
+  console.log(event)
+})
+self.addEventListener('activate', event=>{
+  console.log(event)
+})
+self.addEventListener('fetch',event =>{
+  if(!navigator.onLine){
+      //event.respondWith(new Response("probleme connexion internet"));
+      console.log(event.request.url),
+      console.log(`start server worker`)
+  }
+})
