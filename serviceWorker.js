8dfba542bc4bf672a8f8ed2ef81eaa82770d0bae
Kevin Chan <104557288+KevinChanGH@users.noreply.github.com>
2023-04-08 15:24:28 +0200

    ios and offline support

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,43 @@
-self.addEventListener('install', event=>{
-  console.log(event)
-})
-self.addEventListener('activate', event=>{
-  console.log(event)
-})
-self.addEventListener('fetch',event =>{
-  if(!navigator.onLine){
-      //event.respondWith(new Response("probleme connexion internet"));
-      console.log(event.request.url),
-      console.log(`start server worker`)
-  }
-})
+const cacheName = "PWA-SP";
+const filesToCache = [
+  "/index.html",
+  "/css/style.css",
+  "/js/chat.js",
+  "/images/icon-192x192.png",
+  "/images/icon-256x256.png",
+  "/images/icon-384x384.png",
+  "/images/icon-512x512.png"
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
