c22befa16f87cf164c6a39c6581680a56a8a7349
Kevin Chan <104557288+KevinChanGH@users.noreply.github.com>
2023-04-16 12:10:54 +0200

    Cache + mode offline +Push notification

--- sw.js
+++ sw.js
@@ -1,13 +1,36 @@
-self.addEventListener('install', event=>{
-    console.log(event)
-})
+const staticPWA = "PWA-main";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/chat.js",
+  "/images/icon-192x192.png",
+  "/images/icon-256x256.png",
+  "/images/icon-384x384.png",
+  "/images/icon-512x512.png"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticPWA).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
 self.addEventListener('activate', event=>{
-    console.log(event)
+  console.log(event)
 })
-self.addEventListener('fetch',event =>{
-    if(!navigator.onLine){
-        //event.respondWith(new Response("probleme connexion internet"));
-        console.log(event.request.url),
-        console.log(`start server worker`)
-    }
-})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
+
+self.registration.showNotification ( 'notification SW' , {
+  body : 'Notification depuis mon service workers'
+  });
+
+
